85f95ef4ccc5800e2306a5902faba7f9a2308bea
김동영/서비스플랫폼개발팀/EP <dongyoung.kim@nhnenter.com>
2023-11-08 09:45:29 +0900

    feat: 앱파스 샘플 프로젝트 초기 구성

---
+++ src/main/java/com/apppaas/javaspringbootproject/JavaSpringbootProjectApplication.java
@@ -0,0 +1,13 @@
+package com.apppaas.javaspringbootproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringbootProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringbootProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/apppaas/javaspringbootproject/JavaSpringbootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apppaas.javaspringbootproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringbootProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
